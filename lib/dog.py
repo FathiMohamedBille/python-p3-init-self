01d7264127c98707f3ee6f442d3a9f5804ce3585
fathimohamed <mohafathi24@gmail.com>
2024-08-29 12:39:17 +0300

    done

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 
 class Dog:
+    def __init__(self, name, breed="Mutt", favorite_toy="Any"):
+        self.name = name
+        self.breed = breed
+        self.favorite_toy = favorite_toy
     pass
